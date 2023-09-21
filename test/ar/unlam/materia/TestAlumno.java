68991d20cb3467b5367939e3718ffc4eec7f3e80
diego oliva <diegoliva01@gmail.com>
2023-09-21 10:19:56 -0300

    prueba

--- test/ar/unlam/materia/TestAlumno.java
+++ test/ar/unlam/materia/TestAlumno.java
@@ -2,28 +2,72 @@ package ar.unlam.materia;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+
 import org.junit.Test;
 
 public class TestAlumno {
 
+//	@Test
+//	public void queSePuedaCrearUnAlumno() {
+//
+//		// Preparacion de datos
+//		String nombre = "andres";
+//		String apellido = "Gomez";
+//		Integer dni = 44444;
+//
+//		// ejecucion
+//
+//		Alumno alumno = new Alumno(dni, apellido, nombre);
+//
+//		// validacion
+//
+//		assertNotNull(alumno);
+//
+//		assertEquals(nombre, alumno.getNombre());
+//
+//	}
+	
 	@Test
 	public void queSePuedaCrearUnAlumno() {
-
+		
 		// Preparacion de datos
-		String nombre = "andres";
-		String apellido = "Gomez";
+		String nombre = "Diego";
+		String apellido = "Oliva";
 		Integer dni = 44444;
-
+		Integer id=1;
+		
+		LocalDate nacimiento=LocalDate.of(2001, 7, 28);
+		LocalDate inscripcion=LocalDate.of(2020, 2, 3);
+		
 		// ejecucion
-
-		Alumno alumno = new Alumno(dni, apellido, nombre);
-
+		
+		Alumno alumno = new Alumno(id,nombre, apellido,dni,nacimiento,inscripcion);
+		
 		// validacion
-
+		
 		assertNotNull(alumno);
-
-		assertEquals(nombre, alumno.getNombre());
-
 	}
+	
+//	@Test
+//	public void queSePuedaCrearMasAlumnos() {
+//		
+//		// Preparacion de datos
+//		String nombre = "Diego";
+//		String apellido = "Oliva";
+//		Integer dni = 44444;
+//		Integer id=1;
+//		
+//		LocalDate nacimiento=LocalDate.of(2001, 7, 28);
+//		LocalDate inscripcion=LocalDate.of(2020, 2, 3);
+//		
+//		// ejecucion
+//		
+//		Alumno alumno = new Alumno(id,nombre, apellido,dni,nacimiento,inscripcion);
+//		
+//		// validacion
+//		
+//		assertNotNull(alumno);
+//	}
 
 }
